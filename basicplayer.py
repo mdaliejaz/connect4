b39f820001ebc9dde181f02ec64d45805265da7f
Ali <mdaliejaz@gmail.com>
2015-10-10 18:22:35 -0400

    Update 1

--- basicplayer.py
+++ basicplayer.py
@@ -1,5 +1,6 @@
 from util import memoize, run_search_function
 
+
 def basic_evaluate(board):
     """
     The original focused-evaluate function from the lab.
@@ -18,9 +19,9 @@ def basic_evaluate(board):
         for row in range(6):
             for col in range(7):
                 if board.get_cell(row, col) == board.get_current_player_id():
-                    score -= abs(3-col)
+                    score -= abs(3 - col)
                 elif board.get_cell(row, col) == board.get_other_player_id():
-                    score += abs(3-col)
+                    score += abs(3 - col)
 
     return score
 
@@ -35,6 +36,7 @@ def get_all_next_moves(board):
         except InvalidMoveException:
             pass
 
+
 def is_terminal(depth, board):
     """
     Generic terminal state check, true when maximum depth is reached or
@@ -42,9 +44,10 @@ def is_terminal(depth, board):
     """
     return depth <= 0 or board.is_game_over()
 
-def minimax_find_board_value(board, depth, eval_fn,
-                             get_next_moves_fn=get_all_next_moves,
-                             is_terminal_fn=is_terminal):
+
+def compute_minmax_column_value(board, depth, eval_fn,
+                                get_next_moves_fn=get_all_next_moves,
+                                is_terminal_fn=is_terminal):
     """
     Minimax helper function: Return the minimax value of a particular board,
     given a particular depth to estimate to
@@ -52,20 +55,21 @@ def minimax_find_board_value(board, depth, eval_fn,
     if is_terminal_fn(depth, board):
         return eval_fn(board)
 
-    best_val = None
+    best_alpha_beta = None
 
     for move, new_board in get_next_moves_fn(board):
-        val = -1 * minimax_find_board_value(new_board, depth-1, eval_fn,
-                                            get_next_moves_fn, is_terminal_fn)
-        if best_val == None or val > best_val:
-            best_val = val
+        alpha_beta = -1 * compute_minmax_column_value(new_board, depth - 1, eval_fn,
+                                                      get_next_moves_fn, is_terminal_fn)
+        if best_alpha_beta is None or alpha_beta > best_alpha_beta:
+            best_alpha_beta = alpha_beta
 
-    return best_val
+    return best_alpha_beta
 
-def minimax(board, depth, eval_fn = basic_evaluate,
-            get_next_moves_fn = get_all_next_moves,
-            is_terminal_fn = is_terminal,
-            verbose = True):
+
+def minimax(board, depth, eval_fn=basic_evaluate,
+            get_next_moves_fn=get_all_next_moves,
+            is_terminal_fn=is_terminal,
+            verbose=True):
     """
     Do a minimax search to the specified depth on the specified board.
 
@@ -76,19 +80,19 @@ def minimax(board, depth, eval_fn = basic_evaluate,
     Returns an integer, the column number of the column that the search determines you should add a token to
     """
 
-    best_val = None
+    best_alpha_beta = None
+    best_move = None
 
-    for move, new_board in get_next_moves_fn(board):
-        val = -1 * minimax_find_board_value(new_board, depth-1, eval_fn,
-                                            get_next_moves_fn,
-                                            is_terminal_fn)
-        if best_val == None or val > best_val[0]:
-            best_val = (val, move, new_board)
+    for possible_move, new_board in get_next_moves_fn(board):
+        minmax_value = -1 * compute_minmax_column_value(new_board, depth - 1, eval_fn,
+                                                        get_next_moves_fn,
+                                                        is_terminal_fn)
+        if best_alpha_beta is None or minmax_value > best_alpha_beta:
+            best_alpha_beta = minmax_value
+            best_move = possible_move
 
-    if verbose:
-        print "MINIMAX: Decided on column %d with rating %d" % (best_val[1], best_val[0])
+    return best_move
 
-    return best_val[1]
 
 def rand_select(board):
     """
@@ -99,11 +103,127 @@ def rand_select(board):
     return moves[random.randint(0, len(moves) - 1)]
 
 
+# This function checks for the longest set of tokens in all the
+# possible directions on a row, column and diagonal.
+
+# def get_chain_len(chain_type, board, row, col, player):
+#     token_count = 0
+#     for i in xrange(3):  # Check upto 3 consecutive tokens
+#         if chain_type == 0:
+#             if board.get_cell(row, col + i) == player:
+#                 token_count += 1
+#         elif chain_type == 1:
+#             if board.get_cell(row + i, col) == player:
+#                 token_count += 1
+#         elif chain_type == 2:
+#             if board.get_cell(row + i, col + i) == player:
+#                 token_count += 1
+#     return token_count
+
+
+def get_chain_len(chain_type, board, row, col, player):
+    token_count = 0
+    if chain_type == 0:
+        if col + 2 < 7 and board.get_cell(row, col + 2) == player and board.get_cell(row, col + 1) == player \
+                and board.get_cell(row, col) == player:
+            token_count += 100
+        elif col + 1 < 7 and board.get_cell(row, col + 1) == player and board.get_cell(row, col) == player:
+            token_count += 10
+        elif board.get_cell(row, col) == player:
+            token_count += 1
+    elif chain_type == 1:
+        if row + 2 < 6 and board.get_cell(row + 2, col) == player and board.get_cell(row + 1, col) == player \
+                and board.get_cell(row, col) == player:
+            token_count += 100
+        elif row + 1 < 6 and board.get_cell(row + 1, col) == player and board.get_cell(row, col) == player:
+            token_count += 10
+        elif board.get_cell(row, col) == player:
+            token_count += 1
+    elif chain_type == 2:
+        if row + 2 < 6 and col + 2 < 7 and board.get_cell(row + 2, col + 2) == player and board.get_cell(row + 1, col + 1) == player \
+                and board.get_cell(row, col) == player:
+            token_count += 100
+        elif row + 1 < 6 and col + 1 < 7 and board.get_cell(row + 1, col + 1) == player and board.get_cell(row, col) == player:
+            token_count += 10
+        elif board.get_cell(row, col) == player:
+            token_count += 1
+    return token_count
+
+
+# def new_evaluate(board):
+#     if board.is_game_over():
+#         # If the game has been won, we know that it must have been
+#         # won or ended by the previous move.
+#         # The previous move was made by our opponent.
+#         # Therefore, we can't have won, so return -1000.
+#         # (note that this causes a tie to be treated like a loss)
+#         score = -1000
+#     else:
+#         score = board.longest_chain(board.get_current_player_id()) * 10
+#         # Prefer having your pieces in the center of the board.
+#         current_player = board.get_current_player_id()
+#         other_player = board.get_other_player_id()
+#         for row in xrange(3):
+#             for col in xrange(4):
+#                 score -= max([get_chain_len(i, board, row, col, current_player) for i in xrange(3)])
+#                 score += max([get_chain_len(i, board, row, col, other_player) for i in xrange(3)])
+#     return score
+
+
 def new_evaluate(board):
-    raise NotImplementedError
+    if board.is_game_over():
+        # If the game has been won, we know that it must have been
+        # won or ended by the previous move.
+        # The previous move was made by our opponent.
+        # Therefore, we can't have won, so return -1000.
+        # (note that this causes a tie to be treated like a loss)
+        score = -1000
+    else:
+        score_row = 0
+        score_col = 0
+        score_diag = 0
+        current_player = board.get_current_player_id()
+        other_player = board.get_other_player_id()
+        score = board.longest_chain(current_player)
+        score -= board.longest_chain(other_player) * 10
+
+        for row in xrange(6):
+            for col in xrange(7):
+                # if row < 3 and col < 4:
+                    # Check for consecutive tokens in all three directions
+                    score_col -= get_chain_len(0, board, row, col, current_player)
+                    score_col += get_chain_len(0, board, row, col, other_player)
+
+                    score_row -= get_chain_len(1, board, row, col, current_player)
+                    score_row += get_chain_len(1, board, row, col, other_player)
+
+                    score_diag -= get_chain_len(2, board, row, col, current_player)
+                    score_diag += get_chain_len(2, board, row, col, other_player)
+
+                # elif row >= 3 and col < 4:
+                #     # Check for consecutive tokens in upward direction
+                #     score_row -= get_chain_len(0, board, row, col, current_player)
+                #     score_row += get_chain_len(0, board, row, col, other_player)
+                #
+                # elif col >= 4 and row < 3:
+                #     # Check for consecutive tokens in right direction
+                #     score_col -= get_chain_len(1, board, row, col, current_player)
+                #     score_col += get_chain_len(1, board, row, col, other_player)
+
+                # if board.get_cell(row, col) == board.get_current_player_id():
+                #     score -= score_row + score_col + score_diag
+                # elif board.get_cell(row, col) == board.get_other_player_id():
+                #     score += score_row + score_col + score_diag
+
+        # if board.get_cell(row, col) == board.get_current_player_id():
+        #     score += max(score_row, score_col, score_diag)
+        # elif board.get_cell(row, col) == board.get_other_player_id():
+        #     score -= max(score_row, score_col, score_diag)
+        score += score_row + score_col + score_diag
+    return score
 
 
 random_player = lambda board: rand_select(board)
 basic_player = lambda board: minimax(board, depth=4, eval_fn=basic_evaluate)
-#new_player = lambda board: minimax(board, depth=4, eval_fn=new_evaluate)
+new_player = lambda board: minimax(board, depth=4, eval_fn=new_evaluate)
 progressive_deepening_player = lambda board: run_search_function(board, search_fn=minimax, eval_fn=basic_evaluate)
--- lab3.py
+++ lab3.py
@@ -1,6 +1,7 @@
 # 6.034 Fall 2010 Lab 3: Games
 # Name: <Your Name>
 # Email: <Your Email>
+from array import array
 
 from util import INFINITY
 
@@ -38,14 +39,15 @@ import tree_searcher
 ## grader-bot to play a game!
 ## 
 ## Uncomment this line to play a game as white:
-#run_game(human_player, basic_player)
+# run_game(human_player, basic_player)
 
 ## Uncomment this line to play a game as black:
-#run_game(basic_player, human_player)
-run_game(basic_player, human_player)
+# run_game(basic_player, human_player)
+# run_game(basic_player, human_player)
+
 
 ## Or watch the computer play against itself:
-#run_game(basic_player, basic_player)
+# run_game(basic_player, basic_player)
 
 ## Change this evaluation function so that it tries to win as soon as possible,
 ## or lose as late as possible, when it decides that one side is certain to win.
@@ -57,16 +59,34 @@ def focused_evaluate(board):
     that board is for the current player.
     A return value >= 1000 means that the current player has won;
     a return value <= -1000 means that the current player has lost
-    """    
-    raise NotImplementedError
+    """
+    if board.is_game_over():
+        # If the game has been won, we know that it must have been
+        # won or ended by the previous move.
+        # The previous move was made by our opponent.
+        # Therefore, we can't have won, so return -1000.
+        # (note that this causes a tie to be treated like a loss)
+        score = -1000
+    else:
+        score = 0
+        # Prefer having your pieces in the center of the board.
+        current_player = board.get_current_player_id()
+        other_player = board.get_other_player_id()
+        for row in xrange(3):
+            for col in xrange(4):
+                score += max([get_chain_len(i, board, row, col, current_player) for i in xrange(3)]) ** 2
+                score -= max([get_chain_len(i, board, row, col, other_player) for i in xrange(3)]) ** 2
+
+    return score
 
 
 ## Create a "player" function that uses the focused_evaluate function
 quick_to_win_player = lambda board: minimax(board, depth=4,
                                             eval_fn=focused_evaluate)
 
+
 ## You can try out your new evaluation function by uncommenting this line:
-#run_game(basic_player, quick_to_win_player)
+# run_game(basic_player, quick_to_win_player)
 
 ## Write an alpha-beta-search procedure that acts like the minimax-search
 ## procedure, but uses alpha-beta pruning to avoid searching bad ideas
@@ -82,13 +102,37 @@ def alpha_beta_search(board, depth,
                       # The default functions set here will work
                       # for connect_four.
                       get_next_moves_fn=get_all_next_moves,
-		      is_terminal_fn=is_terminal):
-    raise NotImplementedError
+                      is_terminal_fn=is_terminal):
+    game = None
+    for move, new_board in get_next_moves_fn(board):
+        score = alphabeta(new_board, depth - 1, eval_fn, get_next_moves_fn, is_terminal_fn, NEG_INFINITY, INFINITY)
+        score[0] *= -1
+        if game is None or score[0] > game[0]:
+            game = [score[0], move]
+    return game[1]
+
+
+def alphabeta(board, depth, eval_fn, get_next_moves_fn, is_terminal_fn, alpha, beta):
+    if is_terminal_fn(depth, board):
+        return [eval_fn(board), None]
+
+    score = [NEG_INFINITY, None]
+    for move, new_board in get_next_moves_fn(board):
+        child_score = alphabeta(new_board, depth - 1, eval_fn, get_next_moves_fn, is_terminal_fn,
+                                -beta, -alpha)
+        child_score[0] *= -1
+        alpha = max(alpha, child_score[0])
+        score[0] = alpha
+        score[1] = move
+        if beta >= alpha:
+            return score
+    return score
+
 
 ## Now you should be able to search twice as deep in the same amount of time.
 ## (Of course, this alpha-beta-player won't work until you've defined
 ## alpha-beta-search.)
-#alphabeta_player = lambda board: alpha_beta_search(board,
+# alphabeta_player = lambda board: alpha_beta_search(board,
 #                                                   depth=8,
 #                                                   eval_fn=focused_evaluate)
 alphabeta_player = lambda board: alpha_beta_search(board, depth=4, eval_fn=new_evaluate)
@@ -100,40 +144,41 @@ ab_iterative_player = lambda board: \
     run_search_function(board,
                         search_fn=alpha_beta_search,
                         eval_fn=focused_evaluate, timeout=5)
-#run_game(human_player, alphabeta_player)
+# run_game(human_player, alphabeta_player)
+run_game(alphabeta_player, basic_player)
 
 ## Finally, come up with a better evaluation function than focused-evaluate.
 ## By providing a different function, you should be able to beat
 ## simple-evaluate (or focused-evaluate) while searching to the
 ## same depth.
 
-#def better_evaluate(board):
+# def better_evaluate(board):
 #    raise NotImplementedError
 
 # Comment this line after you've fully implemented better_evaluate
 better_evaluate = memoize(basic_evaluate)
 
 # Uncomment this line to make your better_evaluate run faster.
-# better_evaluate = memoize(better_evaluate)
+better_evaluate = memoize(better_evaluate)
 
 # For debugging: Change this if-guard to True, to unit-test
 # your better_evaluate function.
 if False:
-    board_tuples = (( 0,0,0,0,0,0,0 ),
-                    ( 0,0,0,0,0,0,0 ),
-                    ( 0,0,0,0,0,0,0 ),
-                    ( 0,2,2,1,1,2,0 ),
-                    ( 0,2,1,2,1,2,0 ),
-                    ( 2,1,2,1,1,1,0 ),
+    board_tuples = ((0, 0, 0, 0, 0, 0, 0),
+                    (0, 0, 0, 0, 0, 0, 0),
+                    (0, 0, 0, 0, 0, 0, 0),
+                    (0, 2, 2, 1, 1, 2, 0),
+                    (0, 2, 1, 2, 1, 2, 0),
+                    (2, 1, 2, 1, 1, 1, 0),
                     )
-    test_board_1 = ConnectFourBoard(board_array = board_tuples,
-                                    current_player = 1)
-    test_board_2 = ConnectFourBoard(board_array = board_tuples,
-                                    current_player = 2)
+    test_board_1 = ConnectFourBoard(board_array=board_tuples,
+                                    current_player=1)
+    test_board_2 = ConnectFourBoard(board_array=board_tuples,
+                                    current_player=2)
     # better evaluate from player 1
-    print "%s => %s" %(test_board_1, better_evaluate(test_board_1))
+    print "%s => %s" % (test_board_1, better_evaluate(test_board_1))
     # better evaluate from player 2
-    print "%s => %s" %(test_board_2, better_evaluate(test_board_2))
+    print "%s => %s" % (test_board_2, better_evaluate(test_board_2))
 
 ## A player that uses alpha-beta and better_evaluate:
 your_player = lambda board: run_search_function(board,
@@ -141,25 +186,28 @@ your_player = lambda board: run_search_function(board,
                                                 eval_fn=better_evaluate,
                                                 timeout=5)
 
-#your_player = lambda board: alpha_beta_search(board, depth=4,
+
+# your_player = lambda board: alpha_beta_search(board, depth=4,
 #                                              eval_fn=better_evaluate)
 
 ## Uncomment to watch your player play a game:
-#run_game(your_player, your_player)
+# run_game(your_player, your_player)
 
 ## Uncomment this (or run it in the command window) to see how you do
 ## on the tournament that will be graded.
-#run_game(your_player, basic_player)
+# run_game(your_player, basic_player)
 
 ## These three functions are used by the tester; please don't modify them!
 def run_test_game(player1, player2, board):
     assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
     return run_game(globals()[player1], globals()[player2], globals()[board])
-    
+
+
 def run_test_search(search, board, depth, eval_fn):
     assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
     return globals()[search](globals()[board], depth=depth,
                              eval_fn=globals()[eval_fn])
+
 
 ## This function runs your alpha-beta implementation using a tree as the search
 ## rather than a live connect four game.   This will be easier to debug.
@@ -168,16 +216,16 @@ def run_test_tree_search(search, board, depth):
                              eval_fn=tree_searcher.tree_eval,
                              get_next_moves_fn=tree_searcher.tree_get_next_move,
                              is_terminal_fn=tree_searcher.is_leaf)
-    
+
+
 ## Do you want us to use your code in a tournament against other students? See
 ## the description in the problem set. The tournament is completely optional
 ## and has no effect on your grade.
-COMPETE = (None)
+COMPETE = (True)
 
 ## The standard survey questions.
-HOW_MANY_HOURS_THIS_PSET_TOOK = ""
-WHAT_I_FOUND_INTERESTING = ""
-WHAT_I_FOUND_BORING = ""
-NAME = ""
-EMAIL = ""
-
+HOW_MANY_HOURS_THIS_PSET_TOOK = "34"
+WHAT_I_FOUND_INTERESTING = "blah"
+WHAT_I_FOUND_BORING = "blah"
+NAME = "blah"
+EMAIL = "blah@blah.com"
